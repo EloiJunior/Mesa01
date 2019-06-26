f5a509d667175f5db15c6fe49b37bc41a353f1db
Junior <eloidsjr@hotmail.com>
2019-06-25 21:38:23 -0300

    Add project files.

---
+++ Mesa01/Models/ErrorViewModel.cs
@@ -0,0 +1,11 @@
+using System;
+
+namespace Mesa01.Models
+{
+    public class ErrorViewModel
+    {
+        public string RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
