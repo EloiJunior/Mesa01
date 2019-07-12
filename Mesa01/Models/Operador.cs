15c3ad210f1ff37c4bbebd779a579f81e66cde34
Junior <eloidsjr@hotmail.com>
2019-07-12 19:15:13 -0300

    App locale, number and date formatting

--- Mesa01/Models/Operador.cs
+++ Mesa01/Models/Operador.cs
@@ -1,5 +1,6 @@
 ﻿using System;
 using System.Collections.Generic; //para usar um ICollection, que é uma coleção de Lista etc...
+using System.ComponentModel.DataAnnotations; //para usar o [Display] ou [DataType] ou [DisplayFormat], que é uma tag (anotação) de anotação personalizada, usada para difinir como aparecerá na tela do usuario
 using System.Linq;                //para usar os codigos linq e as expressões lambda
 namespace Mesa01.Models
 {
@@ -7,9 +8,19 @@ namespace Mesa01.Models
     {
         public int Id { get; set; }
         public string Nome { get; set; }
+
+        [DataType(DataType.EmailAddress)]       // anotation usada para transformar o email escrito em link na tela do usuario
         public string Email { get; set; }
+
+        [Display(Name ="Birth Date")]            //tag usada para personalizar como o atributo aparecerá no display, que é a tela do site
+        [DataType(DataType.Date)]                // anotation usada para personalizar como aparecerá os dados na tela
+        [DisplayFormat(DataFormatString = "{0:dd/MM/yyyy}")]  //anotation usada para configurar a data como dia, mes e ano.
         public DateTime BirthDate { get; set; }
+
+        [Display(Name = "Base Salary")]
+        [DisplayFormat(DataFormatString ="{0:F2}")]    // anotation usada para configurar o formato para mostrar os dados, o 0 indica o valor do atributo, F2 indica 2 casas decimais
         public double BaseSalary { get; set; }
+
         public Departamento Departamento { get; set; } //associação de 1 Operador com apenas 1 Departamento
         public int DepartamentoId { get; set; } //foreign key
 
--- Mesa01/Startup.cs
+++ Mesa01/Startup.cs
@@ -2,11 +2,13 @@
 using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
+using System.Globalization;             //para definir a localização de formato de numeros e valores
 using Microsoft.AspNetCore.Builder;
 using Microsoft.AspNetCore.Hosting;
 using Microsoft.AspNetCore.Http;
 using Microsoft.AspNetCore.HttpsPolicy;
 using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Localization; //para definir a localização de formato de numeros e valores
 using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.DependencyInjection;
 using Microsoft.EntityFrameworkCore;
@@ -47,6 +49,19 @@ namespace Mesa01
         // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
         public void Configure(IApplicationBuilder app, IHostingEnvironment env)
         {
+
+            //bloco para configurar a aplicação como sendo dos Estados Unidos
+            var enUS = new CultureInfo("en-US");                         //criamos uma variavel para receber uma Informação de Cultura
+            var localizationOptions = new RequestLocalizationOptions     //criação de objeto com as seguintes configurações:
+            {
+                DefaultRequestCulture = new RequestCulture(enUS),         //qual vai ser o local padrao da minha aplicação
+                SupportedCultures = new List<CultureInfo> { enUS },       // quais são os locais possiveis da minha aplicação
+                SupportedUICultures = new List<CultureInfo> { enUS }      // ??
+            };
+
+            app.UseRequestLocalization(localizationOptions);          //comando que vamos passar o nosso objeto de localização configurado acima
+
+
             if (env.IsDevelopment())
             {
                 app.UseDeveloperExceptionPage();
