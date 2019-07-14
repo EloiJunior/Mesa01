a07ecb19bc8c5cc1d2915fa41d2d13b746928131
Junior <eloidsjr@hotmail.com>
2019-07-14 19:30:47 -0300

    Implementing simple search

--- Mesa01/Controllers/SalesRecordsController.cs
+++ Mesa01/Controllers/SalesRecordsController.cs
@@ -3,20 +3,41 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
 using Microsoft.AspNetCore.Mvc;
+using Mesa01.Services;  //para usar os serviços
 
 namespace Mesa01.Controllers
 {
     public class SalesRecordsController : Controller
     {
+        //injeção de dependencia do serviço
+        private readonly SalesRecordService _salesRecordService;
+        public SalesRecordsController(SalesRecordService salesRecordService)
+        {
+            _salesRecordService = salesRecordService;
+        }
+
+
         public IActionResult Index()
         {
             return View();
         }
 
 
-        public IActionResult SimpleSearch()
+        //Tela SimpleSearch, GET
+        public async Task<IActionResult> SimpleSearch(DateTime? minDate, DateTime? maxDate)
         {
-            return View();
+            if (!minDate.HasValue)  //para aparecer o inicio do ano no filtro de seleçao da tela, qdo não for preenchido manualmente
+            {
+                minDate = new DateTime(DateTime.Now.Year, 1, 1);
+            }
+            if (!maxDate.HasValue)  //para aparecer a data atual no filtro de seleção da tela, qdo não for preenchido manualmente
+            {
+                maxDate = DateTime.Now;
+            }
+            ViewData["minDate"] = minDate.Value.ToString("yyyy-MM-dd");  //para aparecer no filtro a data minima
+            ViewData["maxDate"] = maxDate.Value.ToString("yyyy-MM-dd");  //para aparecer no filtro a data maxima
+            var result = await _salesRecordService.FindByDateAsync(minDate, maxDate);
+            return View(result);
         }
 
 
---
+++ Mesa01/Services/SalesRecordService.cs
@@ -0,0 +1,41 @@
+﻿using Mesa01.Models; //para usar a dependencia do context
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.EntityFrameworkCore; // para usar o Include
+
+namespace Mesa01.Services
+{
+    public class SalesRecordService
+    {
+        //criamos a denpendencia do context:
+        private readonly Mesa01Context_context _context;
+
+        public SalesRecordService(Mesa01Context_context context)
+        {
+            _context = context;
+        }
+
+        //Metodo FindBydate
+        public async Task<List<Fechamento>> FindByDateAsync(DateTime? minDate, DateTime? maxDate)
+        {
+            var result = from obj in _context.Fechamento select obj; //obs. trasforma uma busca do tipo DbSet em um objeto IQueryable: que é aquele objeto que podemos construir as consultas nele
+            if (minDate.HasValue)
+            {
+                result = result.Where(x => x.Data >= minDate.Value);
+            }
+            if (maxDate.HasValue)
+            {
+                result = result.Where(x => x.Data <= maxDate.Value);
+            }
+            //com o return abaixo já mostraria a lista, porem vamos melhorar esse retorno:
+            //return result.ToList();
+            return await result
+                .Include(x => x.Operador)               //join com a tabela de operador
+                .Include(x => x.Operador.Departamento)  //join com o departamento do operador
+                .OrderByDescending(x => x.Data)         //ordenar descendente pela data
+                .ToListAsync();
+        }
+    }
+}
